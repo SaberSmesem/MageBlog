d29ae7847425924a7a6102cb52dbb4d5bb95813b
Saber Said <sabersaid0980@gmail.com>
2022-07-05 17:07:01 +0200

    default route with redirect

---
+++ Controller/Post/Index.php
@@ -0,0 +1,55 @@
+<?php
+namespace SaberSmesem\MageBlog\Controller\Post;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $_pageFactory;
+
+    /**
+     * \SaberSmesem\MageBlog\Model\PostFactory
+     */
+    protected $_postFactory;
+
+    protected $_postsFactory;
+
+    /**
+     * @param \Magento\Framework\App\Action\Context $context
+     */
+    public function __construct(
+       \Magento\Framework\App\Action\Context $context,
+       \Magento\Framework\View\Result\PageFactory $pageFactory,
+       \SaberSmesem\MageBlog\Model\PostFactory $postFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        $this->_postFactory = $postFactory;
+        return parent::__construct($context);
+    }
+    /**
+     * View page action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        /*
+        $post = $this->_postFactory->create();
+		$collection = $post->getCollection();
+		foreach($collection as $item){
+			echo "<pre>";
+			print_r($item->getData());
+			echo "</pre>";
+		}
+		exit();
+        
+        echo "Get Data From sabersmesem_mageblog_posts table";
+
+        die('<br>Read Data');
+        */
+
+        return $this->_pageFactory->create();
+    }
+}
