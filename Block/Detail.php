12442c68a66edfb0418bdb2278aed463f647cd92
Saber Said <sabersaid0980@gmail.com>
2022-07-06 14:53:42 +0200

    show post detail page in frontend

--- Block/Detail.php
+++ Block/Detail.php
@@ -1,21 +1,33 @@
 <?php
 namespace SaberSmesem\MageBlog\Block;
 
+use SaberSmesem\MageBlog\Model\PostFactory;
+
 class Detail extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var SaberSmesem\MageBlog\Model\PostFactory $_postFactory
+     */
+    protected $_postFactory;
+
     /**
      * @param \Magento\Framework\View\Element\Template\Context $context
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        PostFactory $postFactory,
         array $data = []
     ) {
+        $this->_postFactory = $postFactory;
         parent::__construct($context, $data);
     }
 
-    public function getPostDetail()
+    public function getPost()
     {
-        return "Post Details";
+        $request = $this->getRequest()->getParams('id');
+        $post = $this->_postFactory->create()->load($request['id']);
+
+        return $post;
     }
 }
